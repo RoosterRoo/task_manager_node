c32d7cbd42dc3332e08967ef44ff215771f50356
Sayan De <sayande@Sayans-MacBook-Air.local>
2023-03-02 22:50:00 +0530

    Added Custom Error Handling feature

---
+++ errors/custom-error.js
@@ -0,0 +1,15 @@
+class CustomError extends Error {
+    constructor(message, statusCode) {
+        super(message)
+        this.statusCode = statusCode
+    }
+}
+
+const createCustomError = (msg, status) => {
+    return new CustomError(msg, status)
+}
+
+module.exports = {
+    CustomError,
+    createCustomError
+}
--- server.js
+++ server.js
@@ -2,18 +2,21 @@ const express = require('express')
 const path = require('path')
 const taskRouter = require('./routes/tasks/tasks.routes')
 const notFound = require('./middleware/notFound')
+const errorHandler = require('./middleware/error-handling')
 require('dotenv').config()
 const connectDB = require('./db/connect')
 
+
 const app = express()
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname, "public")))
 app.use(express.json())
 
 app.use('/api/v1/tasks', taskRouter)
 app.use(notFound)
+app.use(errorHandler)
 
 const startServer = async () => {
     try {
